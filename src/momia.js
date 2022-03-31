95c229c6e405f9269116966d46204ca5ed080f84
AlLaguna <79445367+AlLaguna@users.noreply.github.com>
2022-03-31 15:12:17 +0200

    Update momia.js

--- src/momia.js
+++ src/momia.js
@@ -1,6 +1,7 @@
 
 import Star from './star.js';
 import Enemy from './enemy.js';
+import { abs } from 'mathjs'
 /**
  * Clase que representa el jugador del juego. El jugador se mueve por el mundo usando los cursores.
  * También almacena la puntuación o número de estrellas que ha recogido hasta el momento.
