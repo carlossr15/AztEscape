bb29fca69bc1fcbcaa972ec67e26a0222fa913f6
AlLaguna <79445367+AlLaguna@users.noreply.github.com>
2022-03-31 15:17:32 +0200

    Update escalera.js

--- src/escalera.js
+++ src/escalera.js
@@ -35,7 +35,7 @@ export default class Escalera extends Phaser.GameObjects.Sprite {
   checkLadder()
   {
     //this.onLadder = false;
-    if(this.scene.physics.overlap(this.scene.player, this) && (this.cursors.up.isDown || this.cursors.down.isDown)){
+    if(this.scene.physics.overlap(this.scene.player, this) && ((this.cursors.up.isDown || this.cursors.down.isDown) || this.scene.player.onLadder)){
       this.scene.player.onLadder = true;
       if(!this.cursors.left.isDown && !this.cursors.right.isDown) this.scene.player.x = this.x + 20;
       console.log("escalera");
